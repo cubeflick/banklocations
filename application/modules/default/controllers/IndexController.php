2bd3e984af5e3d25fd5398de5e03fd5691912f22
manojkumar1989 <manoj.k@cubeflick.com>
2013-03-22 13:21:53 +0530

    minor changes

--- application/modules/default/controllers/IndexController.php
+++ application/modules/default/controllers/IndexController.php
@@ -36,7 +36,7 @@ class IndexController extends App_Controller_BaseController {
     	$this->Model = new Default_Model_Default();
     	$this->view->SectionHeading = "Dashboard";
     	$this->view->BankNames = $this->Model->GetBankNames();
-    	$this->view->navId = 'city';
+    	$this->view->navId = 'ifsc';
     	$this->view->States = $this->Model->GetStatesNames();
     }
     
@@ -45,7 +45,7 @@ class IndexController extends App_Controller_BaseController {
     	$this->Model = new Default_Model_Default();
     	$this->view->SectionHeading = "Dashboard";
     	$this->view->BankNames = $this->Model->GetBankNames();
-    	$this->view->navId = 'city';
+    	$this->view->navId = 'micr';
     	$this->view->States = $this->Model->GetStatesNames();
     }
     
@@ -203,6 +203,114 @@ class IndexController extends App_Controller_BaseController {
     }
     
     public function searchmicrAction() {
+    	$this->view->PageHead = "Search";
+    	$objRequest = $this->getRequest();
+    	$Params = $objRequest->getParams();
+    	$this->view->params = $Params;
+    	$total_pages = $this->Model->getTotalCount($Params);
+    	$adjacents = 3;
+    
+    	unset($Params['module']);
+    	unset($Params['controller']);
+    	unset($Params['action']);
+    	$paramcount = count($Params);
+    	if($paramcount <= 0)
+    	{
+    		$this->_redirect('/');
+    	}
+    	if ($objRequest->isGet()) {
+    		$page = $Params['limitPage'];
+    	} else {
+    		$page = 1;
+    
+    	}
+    	$limit = 10;
+    	if ($page)
+    		$start = ($page - 1) * $limit;
+    	else
+    		$start = 0;
+    
+    	if ($page == 0){
+    		$page = 1;
+    	}
+    	$prev = $page - 1;
+    	$next = $page + 1;
+    	$lastpage = ceil($total_pages / $limit);
+    	$lpm1 = $lastpage - 1;
+    
+    	$pagination = "";
+    	if ($lastpage > 1) {
+    		$pagination .= "<div class=\"pagination\">";
+    		if ($page > 1)
+    			$pagination.= "<a href=\"javascript:void(0)\" onclick=\"setPage('$prev')\">< previous</a>";
+    		else
+    			$pagination.= "<span class=\"disabled\">< previous</span>";
+    
+    		if ($lastpage < 7 + ($adjacents * 2)) {
+    			for ($counter = 1; $counter <= $lastpage; $counter++) {
+    				if ($counter == $page)
+    					$pagination.= "<span class=\"current\">$counter</span>";
+    				else
+    					$pagination.= "<a href=\"javascript:void(0)\" onclick=\"setPage('$counter')\">$counter</a>";
+    			}
+    		}
+    		elseif ($lastpage > 5 + ($adjacents * 2)) {
+    			if ($page < 1 + ($adjacents * 2)) {
+    				for ($counter = 1; $counter < 4 + ($adjacents * 2); $counter++) {
+    					if ($counter == $page)
+    						$pagination.= "<span class=\"current\">$counter</span>";
+    					else
+    						$pagination.= "<a href=\"javascript:void(0)\" onclick=\"setPage('$counter')\">$counter</a>";
+    				}
+    				$pagination.= "<span class=\"dots\">...</span>";
+    				$pagination.= "<a href=\"javascript:void(0)\" onclick=\"setPage('$lpm1')\">$lpm1</a>";
+    				$pagination.= "<a href=\"javascript:void(0)\" onclick=\"setPage('$lastpage')\">$lastpage</a>";
+    			}
+    			elseif ($lastpage - ($adjacents * 2) > $page && $page > ($adjacents * 2)) {
+    				$pagination.= "<a href=\"javascript:void(0)\" onclick=\"setPage('1')\">1</a>";
+    				$pagination.= "<a href=\"javascript:void(0)\" onclick=\"setPage('2')\">2</a>";
+    				$pagination.= "<span class=\"dots\">...</span>";
+    				for ($counter = $page - $adjacents; $counter <= $page + $adjacents; $counter++) {
+    					if ($counter == $page)
+    						$pagination.= "<span class=\"current\">$counter</span>";
+    					else
+    						$pagination.= "<a href=\"javascript:void(0)\" onclick=\"setPage('$counter')\">$counter</a>";
+    				}
+    				$pagination.= "<span class=\"dots\">...</span>";
+    				$pagination.= "<a href=\"javascript:void(0)\" onclick=\"setPage('$lpm1')\">$lpm1</a>";
+    				$pagination.= "<a href=\"javascript:void(0)\" onclick=\"setPage('$lastpage')\">$lastpage</a>";
+    			}
+    			else {
+    				$pagination.= "<a href=\"javascript:void(0)\" onclick=\"setPage('1')\">1</a>";
+    				$pagination.= "<a href=\"javascript:void(0)\" onclick=\"setPage('2')\">2</a>";
+    				$pagination.= "<span class=\"dots\">...</span>";
+    				for ($counter = $lastpage - (2 + ($adjacents * 2)); $counter <= $lastpage; $counter++) {
+    					if ($counter == $page)
+    						$pagination.= "<span class=\"current\">$counter</span>";
+    					else
+    						$pagination.= "<a href=\"javascript:void(0)\" onclick=\"setPage('$counter')\">$counter</a>";
+    				}
+    			}
+    		}
+    
+    		if ($page < $counter - 1)
+    			$pagination.= "<a href=\"javascript:void(0)\" onclick=\"setPage('$next')\">next ></a>";
+    		else
+    			$pagination.= "<span class=\"disabled\">next ></span>";
+    		$pagination.= "</div>\n";
+    	}
+    
+    	$this->view->pagination = $pagination;
+    
+    	$this->view->Records = $this->Model->getBankValues($Params);
+    
+    	$this->view->BankName = '';
+    	if (isset($Params['bank_name']) && $Params['bank_name'] != '' && substr($Params['bank_name'] , 0, 6) != 'Select') {
+    		$this->view->BankName = $Params['bank_name'];
+    	}
+    }
+    
+    public function searchifscAction() {
     	$this->view->PageHead = "Search";
     	$objRequest = $this->getRequest();
     	$Params = $objRequest->getParams();
